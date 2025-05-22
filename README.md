ec8151d36d1437f181c96efc58c10c27355d172a
saisambattula <saisambattula01@gmail.com>
2025-05-22 21:57:13 +0530

    second commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Episodes Wise Learning
+
+Episode 1 ->
+    *
+
+
+Episode 2 ->
+    * learning how create repository in the git and how to push code in to the Git repo
+    * and learning how to configure the npm via package.json
+    * learning about basics about bundlres 
+    * how bundlers make significant change in the react project
+    * various bundlers like babel,parcel,vite,webpack
+    * difference between dev dependencies and normal dependencies Ex:(npm i -d package_name)
+    * difference between package.json and package-lock.jso
+    * breif introduction to nodemodules
+    * ^ and ~ diffrences in dependencies of the package.josn and package-lock.json
+    
---
+++ app.js
@@ -0,0 +1,59 @@
+
+const heading = [React.createElement("div",{id:'parent'},
+    React.createElement('div',{id:'child'},
+        [React.createElement("h1",{},"This is H1 Tag"),
+        React.createElement('h2',{},"this is H2 Tag")]
+    )),
+    React.createElement("div",{id:'parent'},
+        React.createElement('div',{id:'child'},
+            [React.createElement("h1",{},"This is H1 Tag"),
+            React.createElement('h2',{},"this is H2 Tag")]
+        ))
+];
+
+console.log(heading);
+
+
+const root= ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const parent = React.createElement(
+//     "div",{id:'parent'},React.createElement(
+//         "div",{id:"child"},[React.createElement("h1",{id:'h1tag'},"Iam H1 Tag"),React.createElement("h2",{id:"h2tag"},"Iam H2 Tag")]
+//     ))
+//     
+// console.log(parent);
+
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(parent)
